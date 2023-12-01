10e85e8ce252a105bd0ceb4570e20b3e6ea498bb
RadostinC <radostinfc@abv.bg>
2023-12-01 20:49:44 +0200

    In progress

--- RemoveNegativeAndReverse.java
+++ RemoveNegativeAndReverse.java
@@ -1,13 +1,23 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class RemoveNegativeAndReverse {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
-        List<Integer> numbers= Arrays.stream(scanner.nextLine().split(" ")).map(Integer::parseInt).toList();
+        List<Integer> numbers= Arrays.stream(scanner.nextLine().split(" ")).map(Integer::parseInt)
+                .collect(Collectors.toList());
+        for (int i = 0; i <numbers.size() ; i++) {
+            int currentNumber=numbers.get(i);
 
+            if(currentNumber<0){
+                numbers.remove(i);
+            }
+
+        }
+        Collections.reverse(numbers);
+
+        for (int i = 0; i <numbers.size() ; i++) {
+            System.out.printf("%d ",numbers.get(i));
+        }
     }
 }
