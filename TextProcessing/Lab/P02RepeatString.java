b3092eee405ddb631d6e67ccab81bd829b17eaa0
RadostinC <radostinfc@abv.bg>
2024-01-17 00:15:50 +0200

    In progress 1.1

---
+++ TextProcessing/Lab/P02RepeatString.java
@@ -0,0 +1,22 @@
+package TextProcessing.Lab;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P02RepeatString {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int n=Integer.parseInt(scanner.nextLine());
+        String[]words= scanner.nextLine().split(" ");
+        StringBuilder result=new StringBuilder();
+
+        for (String word:words){
+            int count =word.length();
+            for (int i = 0; i <n ; i++) {
+                result.append(word);
+
+            }
+        }
+
+    }
+}
