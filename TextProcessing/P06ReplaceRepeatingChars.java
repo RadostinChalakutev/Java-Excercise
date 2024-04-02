cca051f6f2c1ae8ef9170ac326face9e91933e02
RadostinC <radostinfc@abv.bg>
2024-04-02 23:31:55 +0300

    Completed

---
+++ TextProcessing/P06ReplaceRepeatingChars.java
@@ -0,0 +1,14 @@
+package TextProcessing;
+
+import java.util.Scanner;
+
+public class P06ReplaceRepeatingChars {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        String input=scanner.nextLine();
+
+        for (char symbol:input.toCharArray()){
+            
+        }
+    }
+}
